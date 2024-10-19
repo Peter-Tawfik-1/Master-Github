463fd541e18fb2ce2abeccb0586583d0fa58b6d1
Peter Tawfik <L00186456@atu.ie>
2024-10-19 15:03:01 +0100

    Correcting project name for billing issue

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "peter-bucket-1" {
   name                     = "peter-bucket-tf-1"
-  project                  = "peter-gcp"
+  project                  = "peter-test-2024"
   location                 = "US"
   force_destroy            = false
   public_access_prevention = "enforced"
