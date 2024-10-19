2039aae2d910c6d7218400249cdf04b6bc865e8d
Peter Tawfik <L00186456@atu.ie>
2024-10-19 20:28:38 +0100

    Add compute instance

--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
+/*
+# Create a bucket
 resource "google_storage_bucket" "peter-bucket-1" {
   name                     = "peter-bucket-tf-1"
   project                  = "peter-test-2024"
   location                 = "US"
   force_destroy            = false
   public_access_prevention = "enforced"
+}
+*/
+
+# Create an instance to deploy database on it to simulate deployment
+
+resource "google_compute_instance" "default" {
+  name         = "Database instance"
+  machine_type = "e2-standard-4"
+  project      = "peter-test-2024"
+  zone         = "europe-west2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "rhel-9-v20241009"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
