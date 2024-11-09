2c14e7ced9e1e66eb750586da19efb6e95883b61
Peter Tawfik <L00186456@atu.ie>
2024-11-09 14:35:53 +0000

    Oracle-free-db-install-test-v1

--- main.tf
+++ main.tf
@@ -39,9 +39,19 @@ resource "google_compute_instance" "default" {
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
-    sudo yum update -y
-    sudo yum install -y libaio bc > /tmp/log.txt
-    sudo yum install -y oracle-database-preinstall-21c >> /tmp/log.txt
+    #sudo yum update -y #save time for first run
+    sudo yum install wget -y
+    sudo mkdir sw
+    sudo mkdir log
+    chmod 777 sw
+    chmod 777 log
+    cd sw
+    wget https://yum.oracle.com/repo/OracleLinux/OL9/appstream/x86_64/getPackage/oracle-database-preinstall-23ai-1.0-2.el9.x86_64.rpm
+    wget https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23ai-1.0-1.el9.x86_64.rpm
+    sudo dnf -y install oracle-database-preinstall-23ai-1.0-2.el9.x86_64.rpm > /home/peter/log/install_log.txt
+    sudo dnf install -y oracle-database-free* >> /home/peter/log/install_log.txt
+    export DB_PASSWORD=sys
+    (echo "${DB_PASSWORD}"; echo "${DB_PASSWORD}";) | sudo /etc/init.d/oracle-free-23ai configure >> /home/peter/log/config_log.txt 
   EOF
 }
 
