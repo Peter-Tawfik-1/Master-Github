c471ce0a54e10d38edf3c83d75dbd02f183a4755
Peter Tawfik <L00186456@atu.ie>
2024-11-10 20:52:44 +0000

    test

--- install-oracle-free-version-db.sh
+++ install-oracle-free-version-db.sh
@@ -12,3 +12,17 @@ sudo dnf install -y oracle-database-free* >> /home/peter/log/install_log.txt
 #Note : I am creating sys password as sys to make the DB vulnarable for testing but from the GCP firewall it's not accissable.
 #export DB_PASSWORD=sys
 (echo sys; echo sys;) | sudo /etc/init.d/oracle-free-23ai configure >> /home/peter/log/config_log.txt
+
+
+# In oralce profile follow https://docs.oracle.com/en/database/oracle/oracle-database/23/xeinl/installing-oracle-database-free.html#GUID-A5FCC804-5786-4B4B-B1B4-60E36E80B73F
+Sudo su - oracle
+export ORACLE_SID=FREE 
+export ORAENV_ASK=NO 
+. /opt/oracle/product/23ai/dbhomeFree/bin/oraenv
+
+
+#sudo vi /etc/ssh/sshd_config
+#PasswordAuthentication yes
+#sudo service sshd restart
+ClientAliveInterval  1200
+ClientAliveCountMax 3
