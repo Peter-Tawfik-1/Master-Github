201fce18043ad85576ce1c40e658f5af883840c0
Peter Tawfik <L00186456@atu.ie>
2024-12-06 14:03:47 +0000

    AI pre-requisites

---
+++ Ai-prerequisites.sh
@@ -0,0 +1,4 @@
+sudo yum install python3-pip
+pip install google-cloud-aiplatform # Install the vertexai
+
+gcloud auth application-default login #Authanticate to use AI service after enabling the AI API
